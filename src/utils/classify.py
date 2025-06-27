269d835a8cbfc940fd5cd15e2b611d834c655092
Wei JIANG <wj6768@gadi-login-06.gadi.nci.org.au>
2025-06-27 23:28:59 +1000

    fixed classify bugs

--- src/utils/classify.py
+++ src/utils/classify.py
@@ -1,57 +1,57 @@
 import os
-import envs
+# import envs
 import numpy as np
 import pandas as pd
 from Bio import SeqIO
-from utils import job_management
+# from utils import job_management
 
-def anno_vContact3(prj_dir, config):
-    os.makedirs(os.path.join(prj_dir,"Classification"), exist_ok=True)
-    job_dir = f"{prj_dir}/Classification"
-    log_dir = f"{prj_dir}/Classification"
-    threads = config['pbs']['ncpus']
+# def anno_vContact3(prj_dir, config):
+#     os.makedirs(os.path.join(prj_dir,"Classification"), exist_ok=True)
+#     job_dir = f"{prj_dir}/Classification"
+#     log_dir = f"{prj_dir}/Classification"
+#     threads = config['pbs']['ncpus']
 
-    bash_commands = [
-        f"source {envs.CONDA_PATH}/bin/activate vcontact3",
-        f"{envs.VCONTACT3_PATH} run --nucleotide {prj_dir}/OVU/rep_contigs.fasta --output {prj_dir}/Classification/anno_vcontact3 --db-domain \"prokaryotes\" --db-version {envs.VCONTACT3_DB_VERSION} --db-path {envs.VCONTACT3_DB_PATH} -t {threads}", # -e {{cytoscape,d3js,tree,pyupset,profiles}}
-    ]
-    bash_commands = [x+"\n" for x in bash_commands]
-    if config['job_manager']=='pbs':
-        cluster_job_header = job_management.PBSHeader(
-            job_name="classify_vContact3",
-            ncpus=threads,
-            ngpus=0,
-            mem="32GB",
-            walltime="12:00:00",
-            mail_addr=config['pbs']['mail_addr'],
-            log_o=os.path.join(log_dir, f"{log_dir}/classify_vContact3.o"),
-            log_e=os.path.join(log_dir, f"{log_dir}/classify_vContact3.e"),
-        )
-        cluster_job = job_management.Job(
-            job_manager='pbs',job_header=cluster_job_header, commands=bash_commands,
-        )
-        cluster_job.save_job(job_dir=job_dir)
-    elif config['job_manager']=='gadi':
-        cluster_job_header = job_management.GadiHeader(
-            job_name=f"classify_vContact3",
-            ncpus=threads,
-            ngpus=0,
-            mem="32GB",
-            walltime="12:00:00",
-            mail_addr=config['pbs']['mail_addr'],
-            log_o=os.path.join(log_dir, f"{log_dir}/classify_vContact3.o"),
-            log_e=os.path.join(log_dir, f"{log_dir}/classify_vContact3.e"),
-            project=config['pbs']['gadi']['-P project'],
-            storage=config['pbs']['gadi']['-l storage'],
-            node_type="normalsl",
-            jobfs="2GB",
-        )
-        cluster_job = job_management.Job(
-            job_manager='gadi',job_header=cluster_job_header, commands=bash_commands,
-        )
-        cluster_job.save_job(job_dir=job_dir)
+#     bash_commands = [
+#         f"source {envs.CONDA_PATH}/bin/activate vcontact3",
+#         f"{envs.VCONTACT3_PATH} run --nucleotide {prj_dir}/OVU/rep_contigs.fasta --output {prj_dir}/Classification/anno_vcontact3 --db-domain \"prokaryotes\" --db-version {envs.VCONTACT3_DB_VERSION} --db-path {envs.VCONTACT3_DB_PATH} -t {threads}", # -e {{cytoscape,d3js,tree,pyupset,profiles}}
+#     ]
+#     bash_commands = [x+"\n" for x in bash_commands]
+#     if config['job_manager']=='pbs':
+#         cluster_job_header = job_management.PBSHeader(
+#             job_name="classify_vContact3",
+#             ncpus=threads,
+#             ngpus=0,
+#             mem="32GB",
+#             walltime="12:00:00",
+#             mail_addr=config['pbs']['mail_addr'],
+#             log_o=os.path.join(log_dir, f"{log_dir}/classify_vContact3.o"),
+#             log_e=os.path.join(log_dir, f"{log_dir}/classify_vContact3.e"),
+#         )
+#         cluster_job = job_management.Job(
+#             job_manager='pbs',job_header=cluster_job_header, commands=bash_commands,
+#         )
+#         cluster_job.save_job(job_dir=job_dir)
+#     elif config['job_manager']=='gadi':
+#         cluster_job_header = job_management.GadiHeader(
+#             job_name=f"classify_vContact3",
+#             ncpus=threads,
+#             ngpus=0,
+#             mem="32GB",
+#             walltime="12:00:00",
+#             mail_addr=config['pbs']['mail_addr'],
+#             log_o=os.path.join(log_dir, f"{log_dir}/classify_vContact3.o"),
+#             log_e=os.path.join(log_dir, f"{log_dir}/classify_vContact3.e"),
+#             project=config['pbs']['gadi']['-P project'],
+#             storage=config['pbs']['gadi']['-l storage'],
+#             node_type="normalsl",
+#             jobfs="2GB",
+#         )
+#         cluster_job = job_management.Job(
+#             job_manager='gadi',job_header=cluster_job_header, commands=bash_commands,
+#         )
+#         cluster_job.save_job(job_dir=job_dir)
 
-    return
+#     return
 
 def summarise_OVUs(prj_dir, include=["CAT", "VCT", "GNM"]):
     # include CAT and genomad annotations
@@ -95,67 +95,39 @@ def summarise_OVUs(prj_dir, include=["CAT", "VCT", "GNM"]):
     # include vcontact3 annotations
     def incldue_vContact3(OVU_info_tmp_path, reps_lineage_path):
         ovu_annotations = pd.read_csv(OVU_info_tmp_path, header=0)
-        # vcontact3_summary = pd.read_csv(os.path.join(prj_dir,"Classification","anno_vcontact3","final_assignments.csv"), header=0).fillna("").astype({"family (prediction)":"str"})
-        # vcontact3 = vcontact3_summary[vcontact3_summary["Reference"]==False].drop("index",axis=1).reset_index(drop=True).rename({
-        #     "GenomeName":"representative_contig",
-        #     "realm (prediction)": "realm",
-        #     "phylum (prediction)": "phylum",
-        #     "class (prediction)": "class",
-        #     "order (prediction)": "order",
-        #     "family (prediction)": "family",
-        #     "genus (prediction)": "genus",
-        # },axis=1)
-        vcontact3 = pd.DataFrame({
-            "index":[],
-            "representative_contig":[],
-            "RefSeqID":[],
-            "Proteins":[],
-            "Reference":[],
-            "Size (Kb)":[],
-            "realm (Reference)":[],
-            "realm":[],
-            "phylum (Reference)":[],
-            "phylum":[],
-            "class (Reference)":[],
-            "class":[],
-            "order (Reference)":[],
-            "order":[],
-            "family (Reference)":[],
-            "family":[],
-            "subfamily (Reference)":[],
-            "subfamily":[],
-            "genus (Reference)":[],
-            "genus":[],
-            "network":[],
-        })
-        
-        # vcontact3["vContact3_lineage"] = vcontact3.apply(lambda row: ";".join([row["realm"], row["phylum"], row["class"], row["order"], row["family"], row["genus"]]), axis=1)
-        vcontact3["vContact3_lineage"] = vcontact3.apply(lambda row: "", axis=1)
+        vcontact3_summary = pd.read_csv(os.path.join(prj_dir,"Classification","anno_vcontact3","final_assignments.csv"), header=0).fillna("").astype({"family (prediction)":"str"})
+        vcontact3 = vcontact3_summary.loc[vcontact3_summary["Reference"]==False, :].drop("index",axis=1).reset_index(drop=True).rename({
+            "GenomeName":"representative_contig",
+            "realm (prediction)": "realm",
+            "phylum (prediction)": "phylum",
+            "class (prediction)": "class",
+            "order (prediction)": "order",
+            "family (prediction)": "family",
+            "genus (prediction)": "genus",
+        },axis=1)
 
         reps_lineage = pd.DataFrame({
             "OVU": ovu_annotations["OVU"],
             "representative_contig": ovu_annotations["representative_contig"],
             "cluster_size": ovu_annotations["cluster_size"],
             "CAT_lineage": ovu_annotations["cat_lineage"],#.str.replace("no support",""),
-            # "vContact3_lineage": vcontact3.apply(lambda x: ";".join([str(x["realm"]),str(x["phylum"]),str(x["class"]),str(x["order"]),str(x["family"]),str(x["genus"])]), axis=1),
+            "vContact3_lineage": vcontact3.apply(lambda x: ";".join([str(x["realm"]),str(x["phylum"]),str(x["class"]),str(x["order"]),str(x["family"]),str(x["genus"])]), axis=1),
             "GeNomad_lineage": ovu_annotations["genomad_lineage"],
             "contigs_in_cluster": ovu_annotations["contigs_in_cluster"]
         })
-        reps_lineage = pd.merge(reps_lineage, vcontact3.loc[:, ["representative_contig", "vContact3_lineage"]], how='left', on='representative_contig')
+        # reps_lineage = pd.merge(reps_lineage, vcontact3.loc[:, ["representative_contig", "vContact3_lineage"]], how='left', on='representative_contig')
         reps_lineage = reps_lineage.loc[:,["OVU", "representative_contig","cluster_size","CAT_lineage","vContact3_lineage","GeNomad_lineage","contigs_in_cluster"]]
-        
+        # clean CAT lineage, replace "no support"/"nan" with empty string
         reps_lineage["CAT_lineage"] = reps_lineage["CAT_lineage"].apply(lambda x: ";".join(["" if value in [None, "nan", "no support"] else value for value in str(x).split(";")]))
-        # print(reps_lineage["CAT_lineage"])
+        # replace "*" in CAT lineage with empty string (* is used as recomended by CAT)
         reps_lineage["CAT_lineage"] = reps_lineage["CAT_lineage"].apply(lambda x: ";".join([value.replace("*","") for value in str(x).split(";")]))
-        # print(reps_lineage["CAT_lineage"])
+        # mask bacterial lineage from CAT_pack's results
         reps_lineage["CAT_lineage"] = reps_lineage["CAT_lineage"].apply(lambda x: ";".join([""]*7 if "Viruses" not in str(x).split(";") else str(x).split(";")))
-        # print(reps_lineage["CAT_lineage"])
-        # print(reps_lineage["vContact3_lineage"])
+        # clean vContact3 lineage, replace novel lineage with empty string
         reps_lineage["vContact3_lineage"] = reps_lineage["vContact3_lineage"].apply(lambda x: ";".join(["" if (value in [None, "nan", "no support", "No Realm", "No prediction"]) or ("novel" in value) else value for value in str(x).split(";")]))
-        # print(reps_lineage["vContact3_lineage"])
+        # keep the first part of the lineage if there are multiple parts seperated by "|"
         reps_lineage["vContact3_lineage"] = reps_lineage["vContact3_lineage"].apply(lambda x: ";".join([value.split("|")[0] for value in str(x).split(";")]))
         reps_lineage["GeNomad_lineage"] = reps_lineage["GeNomad_lineage"].apply(lambda x: ";".join(["" if value in [None, "nan", "no support"] else value for value in str(x).split(";")]))
-        # print(reps_lineage["vContact3_lineage"])
         reps_lineage.to_csv(reps_lineage_path, index=None)
         return reps_lineage
 
@@ -202,22 +174,41 @@ def summarise_OVUs(prj_dir, include=["CAT", "VCT", "GNM"]):
                     lineage[list(lineage.keys())[idx]] = value
         return lineage
 
+    # def length_info(OVU_info, quality_filtered_contigs_path):
+    #     contig_id_list = []
+    #     contig_length_list = []
+    #     with open(quality_filtered_contigs_path, 'r') as f:
+    #         for record in SeqIO.parse(f, "fasta"):
+    #             contig_id_list.append(record.id)
+    #             contig_length_list.append(len(record.seq))
+    #     df_contig_length = pd.DataFrame({
+    #         "contig_id": contig_id_list,
+    #         "contig_length": contig_length_list,
+    #     })
+    #     OVU_info["cluster_length"] = OVU_info.apply(lambda s: df_contig_length[df_contig_length["contig_id"].isin(s["contigs_in_cluster"].split(","))]["contig_length"].sum(), axis=1)
+    #     OVU_info["cluster_median_length"] = OVU_info.apply(lambda s: np.median(df_contig_length[df_contig_length["contig_id"].isin(s["contigs_in_cluster"].split(","))]["contig_length"].tolist()), axis=1)
+    #     OVU_info["cluster_mean_length"] = OVU_info["cluster_length"]/OVU_info["cluster_size"]
+    #     OVU_info = OVU_info.loc[:,['OVU', 'representative_contig', 'cluster_size', 'cluster_length', 'cluster_median_length', 'cluster_mean_length', 'lineage', 'contigs_in_cluster']]
+        
+    #     return OVU_info
+    
     def length_info(OVU_info, quality_filtered_contigs_path):
-        contig_id_list = []
-        contig_length_list = []
+        # 先构建 contig_id 到长度的字典，加速查找
+        contig_length_dict = {}
         with open(quality_filtered_contigs_path, 'r') as f:
             for record in SeqIO.parse(f, "fasta"):
-                contig_id_list.append(record.id)
-                contig_length_list.append(len(record.seq))
-        df_contig_length = pd.DataFrame({
-            "contig_id": contig_id_list,
-            "contig_length": contig_length_list,
-        })
-        OVU_info["cluster_length"] = OVU_info.apply(lambda s: df_contig_length[df_contig_length["contig_id"].isin(s["contigs_in_cluster"].split(","))]["contig_length"].sum(), axis=1)
-        OVU_info["cluster_median_length"] = OVU_info.apply(lambda s: np.median(df_contig_length[df_contig_length["contig_id"].isin(s["contigs_in_cluster"].split(","))]["contig_length"].tolist()), axis=1)
-        OVU_info["cluster_mean_length"] = OVU_info["cluster_length"]/OVU_info["cluster_size"]
-        OVU_info = OVU_info.loc[:,['OVU', 'representative_contig', 'cluster_size', 'cluster_length', 'cluster_median_length', 'cluster_mean_length', 'lineage', 'contigs_in_cluster']]
-        
+                contig_length_dict[record.id] = len(record.seq)
+
+        def get_lengths(contigs_str):
+            contigs = contigs_str.split(",")
+            return [contig_length_dict.get(cid, 0) for cid in contigs]
+
+        # 直接用列表推导式批量处理
+        lengths_list = OVU_info["contigs_in_cluster"].map(get_lengths)
+        OVU_info["cluster_length"] = lengths_list.map(sum)
+        OVU_info["cluster_median_length"] = lengths_list.map(lambda x: np.median(x) if x else 0)
+        OVU_info["cluster_mean_length"] = OVU_info["cluster_length"] / OVU_info["cluster_size"]
+        OVU_info = OVU_info.loc[:, ['OVU', 'representative_contig', 'cluster_size', 'cluster_length', 'cluster_median_length', 'cluster_mean_length', 'lineage', 'contigs_in_cluster']]
         return OVU_info
 
     filtered_clusters_path = os.path.join(prj_dir,"OVU","filtered_clusters.tsv")
@@ -225,14 +216,14 @@ def summarise_OVUs(prj_dir, include=["CAT", "VCT", "GNM"]):
     reps_lineage_path = os.path.join(prj_dir,"OVU","reps_lineage.csv")
     OVU_info_path = os.path.join(prj_dir,"OVU","OVU_info.csv")
 
-    include_CAT_genomad(filtered_clusters_path, OVU_info_tmp_path=OVU_info_tmp_path)
+    '''decomment when release'''
+    # include_CAT_genomad(filtered_clusters_path, OVU_info_tmp_path=OVU_info_tmp_path)
     incldue_vContact3(OVU_info_tmp_path=OVU_info_tmp_path, reps_lineage_path=reps_lineage_path)
-    
     reps_lineage = pd.read_csv(reps_lineage_path, header=0)
     tool_name_map = {"CAT":"CAT", "VCT":"vContact3", "GNM":"GeNomad"}
     # lineage_list = [to_lineage(str(x[tool_name_map[tool]+"_lineage"])) for tool in include]
-    # reps_lineage["lineage"] = reps_lineage.apply(lambda x: ";".join(merge_lineage([to_lineage(str(x["CAT_lineage"]),"CAT"),to_lineage(str(x["vContact3_lineage"]),"vContact3"),to_lineage(str(x["GeNomad_lineage"]),"GeNomad")]).values()), axis=1)
     reps_lineage["lineage"] = reps_lineage.apply(lambda x: ";".join(merge_lineage([to_lineage(str(x[tool_name_map[tool]+"_lineage"]), tool_name_map[tool]) for tool in include]).values()), axis=1)
+    reps_lineage = reps_lineage.loc[:,["OVU", "representative_contig","cluster_size","lineage","CAT_lineage","vContact3_lineage","GeNomad_lineage","contigs_in_cluster"]]
     reps_lineage.to_csv(reps_lineage_path, index=None)
 
     OVU_info = reps_lineage.drop(["CAT_lineage","vContact3_lineage","GeNomad_lineage"],axis=1).loc[:,["OVU","representative_contig","cluster_size","lineage","contigs_in_cluster"]]
